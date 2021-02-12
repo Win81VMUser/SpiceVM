a6f596b8d856d2ba1312a3e3bfbdf9bef5ac7e73
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-02-12 22:00:46 +0100

    Update SpiceVM Install Instructions.sh

--- SpiceVM Install Instructions.sh
+++ SpiceVM Install Instructions.sh
@@ -1,10 +1,10 @@
-echo This will install Qemu-system-x86_64-headless and let you host your own VM's into SpiceVM!
+echo "This will install Qemu-system-x86_64-headless and let you host your own VM's into SpiceVM!"
 pkg update
 echo Now installer will install qemu!
 pkg install qemu-system-x86_64-headless
-echo Everything is done! Let's test it! Type qemu-system-x86_64 -monitor stdio -boot n -accel tcg -m 128
-After it loads, open vnc viewer and type 127.0.0.1:5900 as remote desktop and have fun!!!
-If it worked, type exit to exit or press ctrl+c
+echo "Everything is done! Let's test it! Type qemu-system-x86_64 -monitor stdio -boot n -accel tcg -m 128"
+echo "After it loads, open vnc viewer and type 127.0.0.1:5900 as remote desktop and have fun!!!"
+echo "If it worked, type exit to exit or press ctrl+c"
 qemu-system-x86_64 -monitor stdio -boot n -accel tcg -m 128
-echo Now have fun with qemu on your android phone :)
-echo BYE!!!
+echo "Now have fun with qemu on your android phone :)"
+echo "BYE!!!"
