799c4a1c64844ebb3ac765c2b759b225f3894526
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-02-13 08:53:46 +0100

    Update SpiceVM Install Instructions.sh

--- SpiceVM Install Instructions.sh
+++ SpiceVM Install Instructions.sh
@@ -1,7 +1,7 @@
 echo "This will install Qemu-system-x86_64-headless and let you host your own VM's into SpiceVM!"
-pkg update
+pkg update -y
 echo Now installer will install qemu!
-pkg install qemu-system-x86_64-headless
+pkg install qemu-system-x86_64-headless -y
 echo "Everything is done! Let's test it! Type qemu-system-x86_64 -monitor stdio -boot n -accel tcg -m 128"
 echo "After it loads, open vnc viewer and type 127.0.0.1:5900 as remote desktop and have fun!!!"
 echo "If it worked, type quit to exit or press ctrl+c"
